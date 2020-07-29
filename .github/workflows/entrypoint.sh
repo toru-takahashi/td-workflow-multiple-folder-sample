4da66ad3bb0d45da2ea81d1e97cde2e5503e3613
Toru Takahashi <takahashi@treasure-data.com>
2020-07-29 15:09:34 +0900

    update

--- .github/workflows/entrypoint.sh
+++ .github/workflows/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-digdag push project1 --project project1/ $td_wf_endpoint -X client.http.headers.authorization="TD1 $td_master_apikey"
-digdag push project2 --project project2/ $td_wf_endpoint -X client.http.headers.authorization="TD1 $td_master_apikey"
+digdag push project1 --project project1/ --endpoint $td_wf_endpoint -X client.http.headers.authorization="TD1 $td_master_apikey"
+digdag push project2 --project project2/ --endpoint $td_wf_endpoint -X client.http.headers.authorization="TD1 $td_master_apikey"
